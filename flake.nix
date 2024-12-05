3c3412c32a9211ce0e315e279bfd5c4711fbe959
william-ko <bill.kontos@vetspire.com>
2024-12-05 18:37:01 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,172 @@
+{
+  description = "Billy's Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
+        {
+
+          homebrew = {
+            enable = true;
+            casks = [
+              "slack"
+              "arc"
+              "microsoft-teams"
+              "inkdrop"
+              "superhuman"
+              "docker"
+            ];
+            onActivation.cleanup = "zap";
+            onActivation.autoUpdate = true;
+            onActivation.upgrade = true;
+          };
+
+          fonts.packages = [
+            pkgs.nerdfonts
+          ];
+
+          environment.systemPackages = [ pkgs.nixfmt-rfc-style ];
+
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+
+          system.defaults = {
+            dock.autohide = true;
+            NSGlobalDomain.AppleICUForce24HourTime = true;
+            dock.persistent-apps = [
+              "/Applications/Superhuman.app"
+              "/Applications/Arc.app"
+              "/${pkgs.iterm2}/Applications/iTerm2.app"
+              "/Applications/Slack.app"
+              "/Applications/Microsoft Teams.app"
+              "/Applications/Inkdrop.app"
+            ];
+          };
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+
+          users.users.billkontos.home = "/Users/billkontos";
+          home-manager.backupFileExtension = "backup";
+          nix.configureBuildUsers = true;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # programs.fish.enable = true;
+
+          programs.zsh.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#billsmacbook
+      darwinConfigurations."billsmacbook" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.billkontos = import ./home.nix;
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "billkontos";
+
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."billsmacbook".pkgs;
+    };
+}
---
+++ home.nix
@@ -0,0 +1,120 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "billkontos";
+  home.homeDirectory = "/Users/billkontos";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    pkgs.neovim
+    pkgs.iterm2
+    pkgs.mkalias
+    pkgs.duckdb
+    pkgs.direnv
+    pkgs.zellij
+    pkgs.rustup
+    pkgs.nixfmt-rfc-style
+    pkgs.git
+    pkgs.gh
+    pkgs.delta
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".config/nvim".source = ./dotfiles/nvim;
+    ".config/zellij".source = ./dotfiles/zellij;
+    ".gitconfig" = {
+      executable = false;
+      text = ''
+        [user]
+          name = william-ko
+          email = bill.kontos@vetspire.com
+
+        [color]
+          ui = true
+
+        [core]
+          pager = delta
+
+        [interactive]
+          diffFilter = delta --color-only
+
+        [delta]
+          features = side-by-side line-numbers decorations
+          whitespace-error-style = 22 reverse
+
+        [delta "decorations"]
+          commit-decoration-style = bold yellow box ul
+          file-style = bold yellow ul
+          file-decoration-style = none
+
+        [alias]
+          # GH Core aliases
+          gh          = "!f() { gh $@; \n }; f"
+          gist        = "!f() { gh gist $@; \n }; f"
+          issue       = "!f() { gh issue $@; \n }; f"
+          pr          = "!f() { gh pr $@; \n }; f"
+          release     = "!f() { gh release $@; \n }; f"
+          repo        = "!f() { gh repo $@; \n }; f"
+
+          alias       = "!f() { gh alias $@; \n }; f"
+          api         = "!f() { gh api $@; \n }; f"
+          auth        = "!f() { gh auth $@; \n }; f"
+          completion  = "!f() { gh completion $@; \n }; f"
+          config      = "!f() { gh config $@; \n }; f"
+          secret      = "!f() { gh secret $@; \n }; f"
+          ssh-key     = "!f() { gh ssh-key $@; \n }; f"
+
+          # Custom aliases
+          push-origin = "!f() { git push origin -u $(git rev-parse --abbrev-ref HEAD) $@; \n }; f"
+          rewrite     = "!f() { git rebase -i HEAD~$1; \n }; f"
+          gloat       = "!f() { git shortlog -sn; \n }; f"
+          root        = "!f() { git rev-parse --path-format=absolute --show-toplevel; \n }; f"
+          ssh-migrate = "!f() { $HOME/.local/bin/git/ssh-migrate.sh \n }; f"
+      '';
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.direnv.enable = true;
+  programs.direnv.enableZshIntegration = true;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      prmain = "git pull --rebase origin main";
+      prmaster = "git pull --rebase origin master";
+      prstaging = "git pull --rebase origin staging";
+      prprod = "git pull --rebase origin production";
+      amend = "git commit --amend";
+      gco = "git checkout";
+      up = "mix deps.get && mix phx.server";
+      vetapi = "cd ~/Development/vetspire/api && nvim .";
+      vetweb = "cd ~/Development/vetspire/web && nvim .";
+      cdvet = "cd ~/Development/vetspire";
+      cdapi = "cd ~/Development/vetspire/api";
+      cdweb = "cd ~/Development/vetspire/web";
+      z = "zellij";
+    };
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    oh-my-zsh.enable = true;
+    oh-my-zsh.theme = "bira";
+  };
+}
